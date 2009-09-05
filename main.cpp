791effef005f7d8103cda20f454178a0f82b6e88
beru <beru@users.noreply.github.com>
2009-09-05 23:10:21 +0000

    処理の分割、再配置

--- main.cpp
+++ main.cpp
@@ -54,62 +54,135 @@ int _tmain(int argc, _TCHAR* argv[])
 		in[i] = palettes[4 * in[i]];
 	}
 	
-	Quantizer quantizer;
-	quantizer.init(6*8+0, 0, 0, true);
-	
 	const size_t hBlockCount = width / 8 + ((width % 8) ? 1 : 0);
 	const size_t vBlockCount = height / 8 + ((height % 8) ? 1 : 0);
 	const size_t totalBlockCount = hBlockCount * vBlockCount;
 	
-	encode(quantizer, hBlockCount, vBlockCount, &in[0], width, &work[0], width*sizeof(int));
-	
-	if (0) {	
-		int cutoff_table[8][8] = {
-			-1,-1,-1,-1,-1,-1, 1, 1,
-			-1,-1,-1,-1,-1,-1, 1, 1,
-			-1,-1,-1,-1,-1,-1, 1, 1,
-			-1,-1,-1,-1,-1,-1, 1, 1,
-			-1,-1,-1,-1,-1,-1, 1, 1,
-			-1,-1,-1,-1,-1,-1, 1, 1,
-			 1, 1, 1, 1, 1, 1, 1, 1,
-			 1, 1, 1, 1, 1, 1, 1, 1,	
-		};
-		for (size_t i=0; i<8; ++i) {
-			for (size_t j=0; j<8; ++j) {
-				if (cutoff_table[i][j] < 0) {
-					cutoff_table[i][j] = boost::integer_traits<int>::const_max;
-				}
-			}
-		}
-		cutoff(hBlockCount, vBlockCount, &work[0], width*sizeof(int), cutoff_table);
-	}
-	
-	int* pWork = &work[0];
-	
-	reorderByFrequency(hBlockCount, vBlockCount, &work[0], &work2[0]);
-	
-	std::vector<unsigned char> zeroOneInfos(totalBlockCount);
-	unsigned char* pZeroOneInfos = &zeroOneInfos[0];
-	size_t zeroOneLimit = 1;
-	findZeroOneInfos(hBlockCount, vBlockCount, &work2[0], &zeroOneInfos[0], zeroOneLimit);
-	
 	BZip2Compressor compressor;
-	size_t compressedLen = 0;
 	
 	size_t storageSize = work.size()*4*1.1+600;
 	std::vector<unsigned char> work3(storageSize);
 	std::vector<unsigned char> work4(storageSize);
-	std::vector<unsigned char> compressed(storageSize);
+	std::vector<unsigned char> encoded(storageSize);
 	CompressInfo compressInfos[8] = {0};
-	compressedLen = compress(compressor, hBlockCount, vBlockCount, pZeroOneInfos, zeroOneLimit, compressInfos, &work2[0], (unsigned char*)&work[0], &work3[0], &work4[0], &compressed[0], compressed.size());
+	
+	Quantizer quantizer;
+	quantizer.init(6*8+0, 0, 0, false);
+	
+	size_t zeroOneLimit = 1;
+	size_t totalLen = 0;
+	
+	// TODO: to design stream byte formats and various JYEIPEGYUU stream classes and implement serialize methods.
+	
+	// encode
+	{
+		unsigned char* dest = &encoded[0];
+		unsigned char* initialDest = dest;
+		
+		// TODO: to record stream signature "jyeipegyuu\0"
+		// TODO: to record streams
+
+		int* pWork = &work[0];
+		int* pWork2 = &work2[0];
+		
+		// TODO: to record quantizer parameter
+		
+		// TODO: to add encode options such as quantization table.
+		encode(quantizer, hBlockCount, vBlockCount, &in[0], width, pWork, width*sizeof(int));
+		
+		reorderByFrequency(hBlockCount, vBlockCount, pWork, pWork2);
+		
+		// TODO: to add option to disable paeth prediction
+		paethPredictEncode(hBlockCount, vBlockCount, pWork2, pWork);
+		
+		std::vector<unsigned char> signFlags(totalBlockCount*64);
+		unsigned char* pSignFlags = &signFlags[0];
+		size_t signFlagCount = collectInfos(pWork2, totalBlockCount, pSignFlags, compressInfos);
+		
+		std::vector<unsigned char> zeroOneInfos(totalBlockCount);
+		unsigned char* pZeroOneInfos = &zeroOneInfos[0];
+		findZeroOneInfos(hBlockCount, vBlockCount, pWork2, &zeroOneInfos[0], zeroOneLimit);
+		
+		// TODO: to record quantizing zero one limit setting
+		
+		// quantizing zero one flags
+		BitWriter bw(dest+4);
+		for (size_t i=0; i<totalBlockCount; ++i) {
+			bw.putBit(pZeroOneInfos[i]);
+		}
+		*((uint32_t*)dest) = bw.nBytes();
+		dest += 4;
+		dest += bw.nBytes();
+		dest += compress(compressor, hBlockCount, vBlockCount, pZeroOneInfos, zeroOneLimit, compressInfos, pWork2, (unsigned char*)pWork, &work3[0], &work4[0], dest, encoded.size());
+		
+		// TODO: to record DCT coefficients sign predictor setting
+		
+		BitWriter writer(dest+4);
+		for (size_t i=0; i<signFlagCount; ++i) {
+			writer.putBit(signFlags[i] != 0);
+		}
+		*((uint32_t*)dest) = writer.nBytes();
+		dest += 4;
+		dest += writer.nBytes();
+		
+		totalLen = dest - initialDest;
+		
+	}
+	
+	size_t compressedLen = totalLen;
 	
 	std::fill(work.begin(), work.end(), 0);
 	std::fill(work2.begin(), work2.end(), 0);
+	std::vector<unsigned char> tmp(encoded.size());
 	
-	std::vector<unsigned char> tmp(compressed.size());
-	decompress(compressor, hBlockCount, vBlockCount, &compressed[0], compressedLen, &tmp[0], &work[0], &work2[0], work2.size());
-	
-	decode(quantizer, hBlockCount, vBlockCount, &work2[0], width*sizeof(int), &out[0], width);
+	// decode
+	{
+		unsigned char* src = &encoded[0];
+		int* pWork = &work[0];
+		int* pWork2 = &work2[0];
+		size_t destLen = work2.size();
+		
+		// zero one flags
+		uint32_t zeroOneFlagBytes = *(uint32_t*)src;
+		src += 4;
+		std::vector<unsigned char> zeroOneFlags(totalBlockCount);
+		unsigned char* pZeroOneFlags = &zeroOneFlags[0];
+		{
+			BitReader reader(src);
+			assert(totalBlockCount <= zeroOneFlagBytes*8);
+			for (size_t i=0; i<zeroOneFlagBytes*8; ++i) {
+				zeroOneFlags[i] = reader.getBit();
+			}
+		}
+		src += zeroOneFlagBytes;
+		
+		src += decompress(compressor, hBlockCount, vBlockCount, pZeroOneFlags, zeroOneLimit, src, compressedLen, &tmp[0], pWork, pWork2, destLen);
+		
+		// sign flags
+		uint32_t signFlagBytes = *(uint32_t*)src;
+		src += 4;
+		std::vector<unsigned char> signFlags(signFlagBytes * 8);
+		unsigned char* pSignFlags = &signFlags[0];
+		{
+			BitReader reader(src);
+			for (size_t i=0; i<signFlagBytes*8; ++i) {
+				pSignFlags[i] = reader.getBit();
+			}
+			size_t pos = 0;
+			for (size_t i=0; i<totalBlockCount*64; ++i) {
+				int& val = pWork2[i];
+				if (val != 0) {
+					val = pSignFlags[pos++] ? val : -val;
+				}
+			}
+		}
+		
+		paethPredictDecode(hBlockCount, vBlockCount, pWork2, pWork);
+		
+		reorderByPosition(hBlockCount, vBlockCount, pWork2, pWork);
+		
+		decode(quantizer, hBlockCount, vBlockCount, pWork, width*sizeof(int), &out[0], width);
+	}
 	unsigned char* pOutput = &out[0];
 	
 	//FILE* of = _tfopen(_T("out.raw"), _T("wb"));
